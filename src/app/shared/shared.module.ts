2abd7f4c91984efa6a472247fc8b1772038bbe80
Alessandro Ferdian <alessandroferdian128@gmail.com>
2019-10-29 12:31:45 +0700

    Fix Shared Module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule} from '@angular/core';
+import { HeaderDecorComponent } from './header-decor/header-decor.component';
+
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+    HeaderDecorComponent
+  ],
+  exports: [
+    HeaderDecorComponent
+  ]
+})
+
+export class SharedModule { }
