cc7071ea6ba9bb15d6ca39198fbc93fa3dc543d0
Alessandro Ferdian <alessandroferdian128@gmail.com>
2019-10-29 19:44:15 +0700

    News Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  { path: '', redirectTo: 'news', pathMatch: 'full' },
   { path: 'login', loadChildren: './pages/AuthModule/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './pages/AuthModule/register/register.module#RegisterPageModule' },
   { path: 'onboarding', loadChildren: './pages/AuthModule/onboarding/onboarding.module#OnboardingPageModule' },
   { path: 'splash', loadChildren: './pages/splash/splash.module#SplashPageModule' },
+  { path: 'news', loadChildren: './pages/news/news.module#NewsPageModule' },
 ];
 
 @NgModule({
