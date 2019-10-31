165cd1d32dbdbaa7b2bf32e6341c1e517c211451
Alessandro Ferdian <alessandroferdian128@gmail.com>
2019-10-31 12:23:47 +0700

    Add Field Done

--- src/app/pages/addnewfield/addnewfield.page.ts
+++ src/app/pages/addnewfield/addnewfield.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faDirections } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-addnewfield',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addnewfield.page.scss'],
 })
 export class AddnewfieldPage implements OnInit {
-
+  direction = faDirections;
   constructor() { }
 
   ngOnInit() {
