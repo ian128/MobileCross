d3b369229d91741d146c9ba83fa54630d811f27d
Kevin <ckevin2797@gmail.com>
2019-11-21 10:11:15 +0700

    added news.page.ts & news.module.ts

--- src/app/pages/NewsModule/news/news.page.ts
+++ src/app/pages/NewsModule/news/news.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-news',
@@ -10,8 +13,27 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class NewsPage implements OnInit {
   constructor(
     private sanitize: DomSanitizer,
-    private route: Router
-  ) { }
+    private route: Router,
+    public http: HttpClient,
+    public loadingController: LoadingController
+  ) { 
+    this.getNewsList()
+  }
+  newsList:any[]=[]
+
+  async getNewsList()
+  {
+    const loading = await this.loadingController.create({
+      message: 'Hellooo',
+      duration: 2000
+    });
+    await loading.present();
+
+    this.http.get('https://newsapi.org/v2/top-headlines?country=id&category=sports&apiKey=b1374a9fb80a49fe87b4ca8794577a56').subscribe(data=>{
+      this.newsList=data["articles"]
+      loading.dismiss()
+    })
+  }
 
   ngOnInit() {
 
