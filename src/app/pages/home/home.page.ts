ac18ca0b2904d926ce6afae7dae910364e8b47db
Alessandro Ferdian <alessandroferdian128@gmail.com>
2019-11-22 09:26:57 +0700

    Test Geolocation

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -13,9 +17,23 @@ export class HomePage implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.getCurrentPosition();
   }
 
   navTo(input){
     this.router.navigate(['/',input])
   }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    alert(coordinates.coords.latitude);
+    alert(coordinates.coords.longitude);
+  }
+
+  watchPosition() {
+    const wait = Geolocation.watchPosition({}, (position, err) => {
+      console.log(position)
+    })
+  }
+
 }
